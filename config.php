1d8617cb4936b5295d37169126fa10b1ea0a82e2
DAB <dab@dab.la>
2020-07-02 18:06:47 -0400

    initial commit

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'compiler' => [
+        'js' => [
+            'kenwheeler/slick' => [
+                'slick/slick.min.js' => 'slick.min.js',
+            ],
+        ],
+        'css' => [
+            'kenwheeler/slick' => [
+                'slick/slick.scss' => 'slick.css',
+                'slick/slick-theme.scss' => 'slick.css',
+            ],
+        ],
+    ],
+];
